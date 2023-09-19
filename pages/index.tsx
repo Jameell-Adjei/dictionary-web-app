f04416902bedb875fcb270614710149fa69b7014
JayAlmighty00 <jameelladjei@gmail.com>
2023-09-19 18:09:11 -0400

    feat: update the Home component to test API calls

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,17 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+import { getWord, meaningListSelector } from '@/reducers/wordSlice';
+import { useAppDispatch, useAppSelector } from '@/reducers/store';
+import MeaningList from '@/components/MeaningList';
 
 export default function Home() {
+  const dispatch = useAppDispatch()
+  const meaningList = useAppSelector(meaningListSelector);
+   
   return (
     <>
+      <MeaningList/>
+      <button onClick={()=>{dispatch(getWord('dull')); console.log(meaningList)}}>Testing Dispatch to API Call</button> 
     </>
   )
 }
+
+
